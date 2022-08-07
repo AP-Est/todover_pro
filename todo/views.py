ec71dc81d20a68b398c09cd62414f5941ff0fb27
Est <estskif@gmail.com>
2022-08-07 22:51:56 +0400

    Creat autentification system

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,12 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 def signupuser(request):
     if request.method == 'GET':
@@ -23,8 +26,22 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
 
 
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'User not found or password incorrect'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
 def logoutuser(request):
     if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 
 def currenttodos(request):
